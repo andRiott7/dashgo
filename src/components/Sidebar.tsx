39e30dd1fcb031659e8a93d2fa25c0cff949a76f
Thiago Andriotti <T112171@perdigao.com.br>
2022-06-06 13:14:16 -0300

    changing Link tags to NextLink.

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,5 @@
 import { Box, Icon, Link, Stack, Text } from "@chakra-ui/react";
+import NextLink from "next/link";
 import { RiContactsLine, RiDashboardLine, RiGitMergeLine, RiInputMethodLine } from "react-icons/ri";
 
 export function Sidebar() {
@@ -8,28 +9,32 @@ export function Sidebar() {
                 <Box>
                     <Text fontWeight="bold" color="gray.500" fontSize="small" >GERAL</Text>
                     <Stack spacing="4" mt="8" align="stretch">
-                        <Link display="flex" align="center">
-                            <Icon as={RiDashboardLine} fontSize="20" />
-                            <Text fontSize="medium" ml="4">Dashboard</Text>
-
-                        </Link>
-                        <Link display="flex" align="center">
-                            <Icon as={RiContactsLine} fontSize="20" />
-                            <Text fontSize="medium" ml="4">Usuários</Text>
-
-                        </Link>
-
+                        <NextLink href="/dashboard" passHref>
+                            <Link display="flex">
+                                <Icon as={RiDashboardLine} fontSize="20" />
+                                <Text fontSize="medium" ml="4">Dashboard</Text>
+                            </Link>
+                        </NextLink>
+                        <NextLink href="/users" passHref>
+                            <Link display="flex">
+                                <Icon as={RiContactsLine} fontSize="20" />
+                                <Text fontSize="medium" ml="4">Usuários</Text>
+                            </Link>
+                        </NextLink>
                     </Stack>
                 </Box>
                 <Box>
                     <Text fontWeight="bold" color="gray.500" fontSize="small" >AUTOMAÇÃO</Text>
                     <Stack spacing="4" mt="8" align="stretch">
-                        <Link display="flex" align="center">
-                            <Icon as={RiInputMethodLine} fontSize="20" />
-                            <Text fontSize="medium" ml="4">Formulários</Text>
+                        <NextLink href="/users/create" passHref>
+                            <Link display="flex">
+                                <Icon as={RiInputMethodLine} fontSize="20" />
+                                <Text fontSize="medium" ml="4">Formulários</Text>
 
-                        </Link>
-                        <Link display="flex" align="center">
+                            </Link>
+                        </NextLink>
+
+                        <Link display="flex">
                             <Icon as={RiGitMergeLine} fontSize="20" />
                             <Text fontSize="medium" ml="4">Automação</Text>
 
